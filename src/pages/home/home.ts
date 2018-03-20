32822b8d808fb6bf180cbc410e9062cc233d530b
Med Amir Saadallah <amirs-m-s@live.com>
2018-03-20 02:53:07 +0000

    Home basic design done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,21 +2,16 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PopoverController } from 'ionic-angular';
 import { MoreMenuPage } from '../more-menu/more-menu';
-//import { ChangeDetectorRef } from '@angular/core';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  items = [{ 'project': true }, { 'cytoi': false }, { 'contact': false }, { 'about': false }]
-  itemsNames = ['project','cytoi','contact','about']
+  items: any
+  itemsNames = ['commune', 'cytoi', 'contact', 'about']
   constructor(public navCtrl: NavController, private popoverCtrl: PopoverController) {
     //this.items = {'project':false , 'cytoi':false, 'contact':false , 'about':false};
-
-    for (let i = 0; i < this.items.length; i++) {
-      console.log(this.items[i])
-    }
+    this.items = [false,false,false,false]
   }
 
   // show up the top menu
@@ -26,28 +21,18 @@ export class HomePage {
       ev: myEvent
     });
   }
-  // chnage the item from show to hide and inverse
-  public chnageItem(currentitem,event) {
-    //console.log(event);
-    //console.log(currentitem);
-    let targetIndex = this.items[currentitem];
-    let target = targetIndex[this.itemsNames[currentitem]]
-    //console.log(target)
 
-  //    console.log(' incerse it to true')
-      this.items.about = false;
-      this.items.project = false;
-      this.items.contact = false;
-      this.items.cytoi = false;
-      target = !target;
-      //this.items.project= true;
-    //  console.log("new value is "+target)
-    // }
-    // for (let i = 0; i < this.items.length; i++) {
-    //   console.log(this.items[i])
-    // }
-  //  ChangeDetectorRef.detectChanges();
-    return true;
+  // chnage the item from show to hide and inverse
+  public chnageItem(currentitem) {
+    if(this.items[currentitem]){
+      this.items[currentitem] = !this.items[currentitem];
+    }
+    else{
+      for(let i=0;i<this.items.length;i++){
+        this.items[i] = false
+      }
+      this.items[currentitem] = true;
+    }
   }
 
 }
